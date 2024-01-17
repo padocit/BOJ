9a633188f4dc5411e8bff3101e211bef2403382e
lilpado <121782715+lilpado@users.noreply.github.com>
2024-01-17 12:40:31 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	int* arr = new int[N];
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	int max = 0;
+	for (int i = 0; i < N - 2; i++)
+	{
+		for (int j = i + 1; j < N - 1; j++)
+			for (int k = j + 1; k < N; k++)
+			{
+				int sum = arr[i] + arr[j] + arr[k];
+				if (sum > max && sum <= M) max = sum;
+			}
+	}
+	cout << max << endl;
+	return 0;
+}
