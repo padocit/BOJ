43ff5308eb50b9f95e2b840996cbb990b6601684
padosi <121782715+lilpado@users.noreply.github.com>
2024-09-14 02:16:36 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,59 +1,42 @@
-#include <iostream>
-#include <string>
-#include <stack>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+	cin.tie(nullptr);
 
+	stack<int> st;
 	int N;
 	cin >> N;
-
-	stack<int> myStack;
 	while (N-- > 0)
 	{
-		string s;
-		cin >> s;
-		if (s == "push")
+		string cmd;
+		cin >> cmd;
+		if (cmd == "push")
 		{
-			int num; 
-			cin >> num;  // 버퍼에서 가져오는 법은?
-			myStack.push(num);
+			int num;
+			cin >> num;
+			st.push(num);
 		}
-		else if (s == "pop")
+		else if (cmd == "top")
 		{
-			if (myStack.empty())
+			if (st.empty()) cout << -1 << '\n';
+			else cout << st.top() << '\n';
+		}
+		else if (cmd == "pop")
+		{
+			if (st.empty()) cout << -1 << '\n';
+			else
 			{
-				cout << "-1\n";
-				continue;
+				cout << st.top() << '\n'; 
+				st.pop();
 			}
-			cout << myStack.top() << '\n';
-			myStack.pop();
 		}
-		else if (s == "size")
-		{
-			cout << myStack.size() << '\n';
-		}
-		else if (s == "empty")
-		{
-			cout << myStack.empty() << '\n';
-		}
-		else if (s == "top")
-		{
-			if (myStack.empty())
-			{
-				cout << "-1\n";
-				continue;
-			}
-			cout << myStack.top() << '\n';
-		}
-		else
-		{
-			return -1;
-		}
+		else if (cmd == "size") cout << st.size() << '\n';
+		else if (cmd == "empty") cout << st.empty() << '\n';
 	}
+
 	return 0;
 }
